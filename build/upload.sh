3dca2ee49c6225aad5474391eb7c68f09afdb5d2
mli <muli@cs.cmu.edu>
2018-12-27 02:56:38 +0000

    update upload.sh

--- build/upload.sh
+++ build/upload.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-BUCKET=s3://courses.diveintodeeplearning.org/berkeley-stat-157
+BUCKET=s3://courses.d2l.ai/berkeley-stat-157
 DIR=build/_build/html/
 
 aws s3 sync --delete $DIR $BUCKET --acl 'public-read'

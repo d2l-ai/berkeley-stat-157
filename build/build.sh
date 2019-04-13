bc9c92715732a17fe920480babde1f341f4fa49e
Mu Li <muli@cs.cmu.edu>
2019-04-13 15:08:45 -0700

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "============================"
-echo ${EXECUTOR_NUMBER} $((EXECUTOR_NUMBER+1))
-
 git submodule update --init
 conda env update -f build/environment.yml
 # you may need to change to
